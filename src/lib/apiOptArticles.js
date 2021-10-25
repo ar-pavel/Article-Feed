a6932f22180ef5b6f0b71a817d49dde96190c064
ar-pavel <atikur@cefalo.com>
2021-10-25 11:05:32 +0600

    converted to async/await

--- src/lib/apiOptArticles.js
+++ src/lib/apiOptArticles.js
@@ -1,26 +1,27 @@
 
 export async function getArticle (id) {
-    const res = await fetch(`http://localhost:8084/articles/${id}`, {
+    let res = await fetch(`http://localhost:8084/articles/${id}`, {
         method: 'GET',
         mode: 'cors',
         headers: {
             'Content-Type': 'application/json'
         }
-    }).then(data => data.json())
-    console.log("response:", res);
+    });     
+    res = res.json();
+    // console.log("response:", res);
     return res;
 }
 
 
 export async function getArticles (){
-    const res = await fetch('http://localhost:8084/articles', {
+    let res = await fetch('http://localhost:8084/articles', {
         method: 'GET',
         mode: 'cors',
         headers: {
             'Content-Type': 'application/json'
         }
     })
-        .then(data => data.json())
+    res = res.json();
     console.log("response:", res);
     return res;
 }
