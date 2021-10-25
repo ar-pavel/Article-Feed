5cc81ad63c9cd24f9452254fe1403927bd9452c0
ar-pavel <atikur@cefalo.com>
2021-10-25 12:01:17 +0600

    initialized new variable insted of resuing one

--- src/lib/apiOptArticles.js
+++ src/lib/apiOptArticles.js
@@ -1,15 +1,15 @@
 export async function getArticles (id=null){
 
-    let res = await fetch(id?`http://localhost:8084/articles/${id}`:'http://localhost:8084/articles', {
+    const res = await fetch(id?`http://localhost:8084/articles/${id}`:'http://localhost:8084/articles', {
         method: 'GET',
         mode: 'cors',
         headers: {
             'Content-Type': 'application/json'
         }
     })
-    res = res.json();
-    console.log("response:", res);
-    return res;
+    const json = res.json();
+    // console.log("response:", json);
+    return json;
 }
 
 
