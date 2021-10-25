870182280a77949924633671a41e8f514ea1ce56
ar-pavel <atikur@cefalo.com>
2021-10-25 13:16:20 +0600

    updated

--- src/hook/useToken.js
+++ src/hook/useToken.js
@@ -1,14 +1,11 @@
 import {useState} from 'react'
 
-// using session storage to store the token 
 const useToken = () => {
 
     const getToken = () => {
         const tokenString = sessionStorage.getItem('token');
         const userToken = JSON.parse(tokenString);
-         // optional chaining operator '?.' for avoiding error in accessing the token property before storing, 
-        // in that case the value of sessionStorage.getItem('token') will be undefined
-        return userToken?.token;
+        return userToken;
     };
     
     const [token, setToken] = useState(getToken());
@@ -16,8 +13,8 @@ const useToken = () => {
     const saveToken = userToken => {
         sessionStorage.setItem('token', JSON.stringify(userToken));
         setToken(userToken);
-        console.log("token to saved:", userToken)
-        console.log("token saved:", token)
+        //console.log("token to saved:", userToken)
+        //console.log("token saved:", token)
     }
 
     return {
