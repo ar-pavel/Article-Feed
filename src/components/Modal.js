640350a0a841aad93d682e5cbad440c3b789fe8a
ar-pavel <atikur@cefalo.com>
2021-10-26 13:57:42 +0600

    Generic modal implemented

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,47 +1,52 @@
-
 const Modal = ({ handleClose, show, children }) => {
+  const style = {
+    modal: {
+      position: "fixed",
+      top: "0",
+      left: "0",
+      width: "100%",
+      height: " 100%",
+      background: "rgba(0, 0, 0, 0.6)",
+    },
 
-    const style = {
-        modal: {
-            position: "fixed",
-            top: 0,
-            left: 0,
-            width:"100%",
-            height:" 100%",
-            background: rgba(0, 0, 0, 0.6)
-        },
-        
-        main: {
-            position:"fixed",
-            background: "white",
-            width: 80%,
-            height: "auto",
-            top:"50%",
-            left:"50%",
-            transform: "translate(-50%,-50%)",
-        },
-        
-        displayBlock: {
-            display: "block"
-        },
-        
-        displayNone: {
-            display: "none"
-        }
-    }
-    
-    const showHide = show ? style.displayBlock : style.displayNone;
-    
-    return (
-      <div style={showHide}>
+    main: {
+      padding: "10px",
+      position: "fixed",
+      background: "white",
+      width: "80 %",
+      height: "auto",
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%,-50%)",
+    },
+
+    displayBlock: {
+      display: "block",
+    },
+
+    displayNone: {
+      display: "none",
+    },
+  };
+
+  const showHide = show ? style.displayBlock : style.displayNone;
+
+  return (
+    <main style={showHide}>
+      <div style={style.modal}>
         <section style={style.main}>
           {children}
-          <button type="button" onClick={handleClose}>
+          <button
+            style={{ float: "right" }}
+            type="button"
+            onClick={handleClose}
+          >
             Close
           </button>
         </section>
       </div>
-    );
+    </main>
+  );
 };
 
 export default Modal;
