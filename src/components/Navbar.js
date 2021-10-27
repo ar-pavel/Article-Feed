08755a3862d5e0743120ad7b993523329e2992f6
ar-pavel <atikur@cefalo.com>
2021-10-27 12:37:55 +0600

    add article button added in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,43 @@
 import { Link } from "react-router-dom";
 import useToken from "../hook/useToken";
 
-const Navbar = ({left, right})=>{
+const Navbar = ({ left, right }) => {
+  const { userName } = useToken();
 
-    const {userName} = useToken();
+  const style = {
+    flex: {
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+    },
+    button: {
+      margin: "10px",
+    },
+  };
 
-    const style = {
-        flex: { display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "space-between" }
-    };
-
-    return(<> 
+  return (
+    <>
+      <div style={style.flex}>
         <div style={style.flex}>
-            <div style={style.flex}>
-                {left}
-                {right}
-            </div>
-            <div>
-                {userName? <p>{userName}</p> : <Link style={{textDecoration: "none"}} to={'/login'} ><p>login</p> </Link>}
-            </div>
+          {left}
+          {right}
         </div>
-        
-    </>);
-
-}
+        <div>
+          {userName ? (
+            <div style={style.flex}>
+              <button style={style.button}>Add Article</button>
+              <p>{userName}</p>
+            </div>
+          ) : (
+            <Link style={{ textDecoration: "none" }} to={"/login"}>
+              <p>login</p>{" "}
+            </Link>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
