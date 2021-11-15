e64b5b2d5a8df02c1d99f823c056c3f2b663bc0f
ar-pavel <atikur@cefalo.com>
2021-11-15 10:34:26 +0600

    router structure chagned

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Redirect, useHistory, useParams } from "react-router";
+import { useHistory, useParams } from "react-router";
 import { Link } from "react-router-dom";
 import UpdaterContext from "../hook/updaterContext";
 import useToken from "../hook/useToken";
@@ -22,27 +22,23 @@ const Article = () => {
   const { userName, token } = useToken();
   const [status, setStatus] = useState(false);
 
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     const fetch = async () => {
       try {
         const data = await fetchData(`/articles/${article.uuid}`, "GET");
         console.log("data fetched :", data);
         setArticle(data);
+        setError(false);
       } catch (error) {
         console.log("unable to fetch");
-        setArticle({
-          uuid: null,
-          title: "",
-          description: "",
-          author: "",
-        });
+        setError(true);
       }
       console.log("update status:", updateStatus);
     };
     fetch();
-    // not sure why removing `article.title, article.description` as dependancy
-    // doesn't always work
-  }, [updateStatus]);
+  }, [article.uuid, updateStatus]);
 
   const handleDelete = () => {
     try {
@@ -53,9 +49,7 @@ const Article = () => {
     }
   };
 
-  return article.uuid == null ? (
-    <Redirect to="/404" />
-  ) : (
+  return (
     <>
       <Navbar
         left={
@@ -67,75 +61,77 @@ const Article = () => {
         }
       />
 
-      <div className="article-view">
-        <div className="flex-display space-between">
-          <h2 className="title"> {article.title}</h2>
+      {error ? (
+        <h1>NO CONTENT</h1>
+      ) : (
+        <div className="article-view">
+          <div className="flex-display space-between">
+            <h2 className="title"> {article.title}</h2>
+            {userName === article.author ? (
+              <>
+                <button
+                  className="article-edit-button flex-display space-between"
+                  disabled={status}
+                  onClick={() => {
+                    setStatus(true);
+                  }}
+                >
+                  <img
+                    className="add-logo"
+                    src={process.env.PUBLIC_URL + "/edit.png"}
+                    height={15}
+                    width={15}
+                    alt="LOGO"
+                  />
+                  <p> Edit</p>
+                </button>
 
-          {userName === article.author ? (
-            <>
-              <button
-                className="article-edit-button flex-display space-between"
-                disabled={status}
-                onClick={() => {
-                  setStatus(true);
-                }}
-              >
-                <img
-                  className="add-logo"
-                  src={process.env.PUBLIC_URL + "/edit.png"}
-                  height={15}
-                  width={15}
-                  alt="LOGO"
-                />
-                <p> Edit</p>
-              </button>
-
-              <button
-                className="article-delete-button flex-display space-between"
-                disabled={status}
-                onClick={() => {
-                  window.confirm(
-                    "Are you sure you wish to delete this item?"
-                  ) && handleDelete();
-                }}
-              >
-                <img
-                  className="add-logo"
-                  src={process.env.PUBLIC_URL + "/delete-file.png"}
-                  height={15}
-                  width={15}
-                  alt="LOGO"
-                />
-                <p> Delete</p>
-              </button>
-            </>
-          ) : null}
+                <button
+                  className="article-delete-button flex-display space-between"
+                  disabled={status}
+                  onClick={() => {
+                    window.confirm(
+                      "Are you sure you wish to delete this item?"
+                    ) && handleDelete();
+                  }}
+                >
+                  <img
+                    className="add-logo"
+                    src={process.env.PUBLIC_URL + "/delete-file.png"}
+                    height={15}
+                    width={15}
+                    alt="LOGO"
+                  />
+                  <p> Delete</p>
+                </button>
+              </>
+            ) : null}
+          </div>
+          <div>
+            <p>
+              By <Link to="#"> {article.author} </Link>
+            </p>
+            {/*This section is just a placeholder*/}
+            <p>
+              <span className="flex-display rand">
+                <span className="timestamp">
+                  <span>Last Validated on</span>
+                  <span>March 30, 2020</span>
+                </span>
+                <span className="timestamp">
+                  <span>Originally Published on</span>
+                  <span>March 26, 2020</span>
+                </span>
+                <span>
+                  <span>161.5k</span>
+                  <span> views</span>
+                </span>
+              </span>
+            </p>
+          </div>
+          <div className="description">{article.description}</div>
         </div>
-        <div>
-          <p>
-            By <Link to="#"> {article.author} </Link>
-          </p>
-          {/*This section is just a placeholder*/}
-          <p>
-            <span className="flex-display rand">
-              <span className="timestamp">
-                <span>Last Validated on</span>
-                <span>March 30, 2020</span>
-              </span>
-              <span className="timestamp">
-                <span>Originally Published on</span>
-                <span>March 26, 2020</span>
-              </span>
-              <span>
-                <span>161.5k</span>
-                <span> views</span>
-              </span>
-            </span>
-          </p>
-        </div>
-        <div className="description">{article.description}</div>
-      </div>
-
+      )}
       {status && (
         <ConstructArticle article={article} changeStatus={setStatus} />
       )}
